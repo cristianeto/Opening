1367ebd0c6a4b27c088fbed19b191f33e691b457
Cristian Guamán <Cristian@McBkPro-Cristian.local>
2014-09-29 18:03:10 -0500

    Initial Commit

---
+++ Opening/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Opening
+//
+//  Created by Cristian Guamán on 29/09/14.
+//  Copyright (c) 2014 ESPOCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Opening/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Opening
+//
+//  Created by Cristian Guamán on 29/09/14.
+//  Copyright (c) 2014 ESPOCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
